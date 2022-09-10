744627003e29895caf3aaee4c25720f8c587b698
Liam Healey <56327428+LiamDHealey@users.noreply.github.com>
2022-09-10 13:51:04 -0600

    Create file

---
+++ Source/Syrup/FieldsManager.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FieldsManager.h"
+
+// Sets default values
+AFieldsManager::AFieldsManager()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AFieldsManager::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFieldsManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
