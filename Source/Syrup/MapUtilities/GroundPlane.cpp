df0252b2bdb85d7912653997186208b045e75980
Liam Healey <56327428+LiamDHealey@users.noreply.github.com>
2022-09-10 21:36:25 -0600

    Scale back ground plane scope

---
+++ Source/Syrup/MapUtilities/GroundPlane.cpp
@@ -0,0 +1,119 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GroundPlane.h"
+
+#include "Components/InstancedStaticMeshComponent.h"
+#include "Syrup/Tiles/GridLibrary.h"
+
+
+/**
+ * Applies one layer of a field in a given area.
+ *
+ * @param FieldType - The type of the field. Fields of different types are independent.
+ * @param Locations - The locations to add the field in.
+ */
+void AGroundPlane::ApplyField(EFieldType Type, TSet<FIntPoint> Locations)
+{
+	AddFieldStrength(Type, 1, Locations);
+}
+
+/**
+ * Removes one layer of field from a given area.
+ *
+ * @param FieldType - The type of the field. Fields of different types are independent.
+ * @param Locations - The locations to remove the field from.
+ */
+void AGroundPlane::RemoveField(EFieldType Type, TSet<FIntPoint> Locations)
+{
+	AddFieldStrength(Type, -1, Locations);
+}
+
+AGroundPlane::AGroundPlane()
+{
+	RootComponent = CreateDefaultSubobject<USceneComponent>(FName("Root"));
+
+	//Get Tile Mesh
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> MeshRef(TEXT("/Game/Tiles/SM_Tile.SM_Tile"));
+	UStaticMesh* TileMesh = MeshRef.Object;
+	check(TileMesh != nullptr);
+
+	//Create ground plane
+	GroundMesh = CreateDefaultSubobject<UInstancedStaticMeshComponent>(FName("GroundPlane"));
+	GroundMesh->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+	GroundMesh->SetAbsolute(true, true, true);
+	GroundMesh->SetStaticMesh(TileMesh);
+	GroundMesh->SetMaterial(0, Material);
+	GroundMesh->CastShadow = false;
+	GroundMesh->NumCustomDataFloats = 2;
+}
+
+/**/
+void AGroundPlane::OnConstruction(const FTransform& Transform)
+{
+	if (LocationsToInstanceIndices.Num() != PlaneSize.X * PlaneSize.Y)
+	{
+		GroundMesh->ClearInstances();
+		LocationsToInstanceIndices.Empty();
+
+		for (int IndexX = -(PlaneSize.X / 2); IndexX < PlaneSize.X / 2 + PlaneSize.X % 2; IndexX++)
+		{
+			for (int IndexY = -(PlaneSize.Y / 2); IndexY < PlaneSize.Y / 2 + PlaneSize.Y % 2; IndexY++)
+			{
+				FIntPoint GridLocation = FIntPoint(IndexX, IndexY);
+				LocationsToInstanceIndices.Add(GridLocation, GroundMesh->AddInstance(UGridLibrary::GridTransformToWorldTransform(FGridTransform(GridLocation)) * FTransform(FVector(0, 0, -1)), true));
+			}
+		}
+	}
+}
+
+/**
+ * Edits a field's strength in the given area.
+ *
+ * @param FieldType - The type of the field. Fields of different types have independent strengths.
+ * @param Strength - The value to add to the field strength. Note: Field values clamped to >= 0.
+ * @param Locations - The locations to change the field strength in.
+ */
+void AGroundPlane::AddFieldStrength(const EFieldType FieldType, const int Strength, const TSet<FIntPoint> Locations)
+{
+	//Create field map if not it doesn't exist
+	if (!FieldTypeToLocationToStrengths.Contains(FieldType))
+	{
+		FieldTypeToLocationToStrengths.Add(FieldType, TMap<FIntPoint, int>());
+	}
+
+	for (FIntPoint EachLocation : Locations)
+	{
+		//Skip if outside domain
+		if (!LocationsToInstanceIndices.Contains(EachLocation))
+		{
+			continue;
+		}
+
+		//Create new entry if needed
+		if (!FieldTypeToLocationToStrengths.Find(FieldType)->Contains(EachLocation))
+		{
+			if (Strength > 0)
+			{
+				FieldTypeToLocationToStrengths.Find(FieldType)->Add(EachLocation, Strength);
+				GroundMesh->SetCustomDataValue(LocationsToInstanceIndices.FindRef(EachLocation), (uint8)FieldType, 1, true);
+			}
+		}
+		else
+		{
+			//Change strength a location
+			int NewValue = FieldTypeToLocationToStrengths.Find(FieldType)->FindRef(EachLocation);
+			NewValue += Strength;
+			if (NewValue <= 0)
+			{
+				//Eliminate value at location if field is too weak.
+				FieldTypeToLocationToStrengths.Find(FieldType)->Remove(EachLocation);
+				GroundMesh->SetCustomDataValue(LocationsToInstanceIndices.FindRef(EachLocation), (uint8)FieldType, 0, true);
+			}
+			else
+			{
+				FieldTypeToLocationToStrengths.Find(FieldType)->Add(EachLocation, NewValue);
+			}
+		}
+	}
+}
---
+++ Source/Syrup/MapUtilities/GroundPlane.h
@@ -0,0 +1,92 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GroundPlane.generated.h"
+
+class ATile;
+
+/* \/ ========== \/ *\
+|  \/ EFieldType \/  |
+\* \/ ========== \/ */
+UENUM(BlueprintType)
+enum class EFieldType : uint8
+{
+	Protection	UMETA(DisplayName = "Protection Radius"),
+	Damage		UMETA(DisplayName = "Damage Radius")
+};
+/* /\ ========== /\ *\
+|  /\ EFieldType /\  |
+\* /\ ========== /\ */
+
+/* \/ ============== \/ *\
+|  \/ AGroundPlane \/  |
+\* \/ ============== \/ */
+/**
+ * An object for portraying the ground plain.
+ * Has functions for receiving field data.
+ */
+UCLASS()
+class SYRUP_API AGroundPlane : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	//The size of the grid to track fields in.
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FIntPoint PlaneSize = FIntPoint(100, 100);
+
+	/**
+	 * Applies a field in a given area.
+	 *
+	 * @param FieldType - The type of the field. Fields of different types are independent.
+	 * @param Locations - The locations to add the field in.
+	 */
+	UFUNCTION(BlueprintCallable)
+	void ApplyField(EFieldType Type, TSet<FIntPoint> Locations);
+
+	/**
+	 * Removes a field from a given area.
+	 *
+	 * @param FieldType - The type of the field. Fields of different types are independent.
+	 * @param Locations - The locations to remove the field from.
+	 */
+	UFUNCTION(BlueprintCallable)
+	void RemoveField(EFieldType Type, TSet<FIntPoint> Locations);
+
+	AGroundPlane();
+	virtual void OnConstruction(const FTransform& Transform) override;
+	virtual void BeginPlay() override;
+
+protected:
+	//The plane used to render the fields
+	UPROPERTY()
+	UInstancedStaticMeshComponent* GroundMesh;
+
+	//The material to be applied to the ground
+	UPROPERTY(EditAnywhere)
+	UMaterialInterface* Material;
+
+private:
+	/**
+	 * Edits a field's strength in the given area.
+	 * 
+	 * @param FieldType - The type of the field. Fields of different types have independent strengths.
+	 * @param Strength - The value to add to the field strength. Note: Field values clamped to >= 0.
+	 * @param Locations - The locations to change the field strength in.
+	 */
+	UFUNCTION(BlueprintCallable)
+	void AddFieldStrength(const EFieldType FieldType, const int Strength, const TSet<FIntPoint> Locations);
+
+	//Stores the grid locations of each ground mesh instance
+	UPROPERTY(Transient)
+	TMap<FIntPoint, int32> LocationsToInstanceIndices = TMap<FIntPoint, int32>();
+
+	//Stores strengths of all fields.
+	TMap<EFieldType, TMap<FIntPoint, int>> FieldTypeToLocationToStrengths = TMap<EFieldType, TMap<FIntPoint, int>>();
+};
+/* /\ ============== /\ *\
+|  /\ AGroundPlane /\  |
+\* /\ ============== /\ */
