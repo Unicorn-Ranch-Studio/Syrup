6763d6b324fe81bd4de854ed035c4cc906bca15d
Liam Healey <56327428+LiamDHealey@users.noreply.github.com>
2022-09-10 18:10:38 -0600

    Fields Manager Implemented

--- Source/Syrup/FieldsManager.h
+++ Source/Syrup/FieldsManager.h
@@ -34,8 +34,14 @@ class SYRUP_API AFieldsManager : public AActor
 	GENERATED_BODY()
 	
 public:	
-	
+	//The size of the grid to track fields in.
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FIntPoint FieldDomain = FIntPoint(100, 100);
+
+
 	AFieldsManager();
+	virtual void OnConstruction(const FTransform& Transform) override;
+	virtual void BeginPlay() override;
 
 	/**
 	 * Edits a field's strength in the given area.
@@ -44,6 +50,7 @@ public:
 	 * @param Strength - The value to add to the field strength. Note: Field values clamped to >= 0.
 	 * @param Locations - The locations to change the feild strength in.
 	 */
+	UFUNCTION(BlueprintCallable)
 	void AddFieldStrength(const EFieldType FieldType, const int Strength, const TSet<FIntPoint> Locations);
 
 	/**
@@ -53,15 +60,36 @@ public:
 	 * @param Location - The locations to change the feild strength in.
 	 * @return The strength of the field at the given location. Field strength will be 0 if the field is not present.
 	 */
+	UFUNCTION(BlueprintPure)
 	int GetFieldStrengthAtLocation(const EFieldType FieldType, const FIntPoint Location) const;
 
 	/**
 	 * Gets the set of all tiles in a given field.
 	 *
 	 * @param Type - The type of the field to query.
-	 * @return The set of all tiles in a given field.
+	 * @return A map of effected tiles and the maximum field strength that tile is in.
 	 */
-	TSet<ATile*> GetTilesInField(const EFieldType FieldType) const;
+	UFUNCTION(BlueprintPure)
+	TMap<ATile*, int> GetTilesInField(const EFieldType FieldType);
+
+protected:
+	//The plane used to render the fields
+	UPROPERTY()
+	UInstancedStaticMeshComponent* GroundPlane;
+
+	//The material to be applied to the ground
+	UPROPERTY(EditAnywhere)
+	UMaterialInterface* GroundMaterial;
+
+private:
+	UPROPERTY(Transient)
+	TMap<FIntPoint, int32> LocationsToInstanceIndices = TMap<FIntPoint, int32>();
+
+	//Stores strengths of all fields.
+	TMap<EFieldType, TMap<FIntPoint, int>> FieldTypeToLocationToStrengths = TMap<EFieldType, TMap<FIntPoint, int>>();
+
+	//Stores the most recent valid tile to strength maps.
+	TMap<EFieldType, TMap<ATile*, int>> FieldTypeToTilesToStrengths = TMap<EFieldType, TMap<ATile*, int>>();
 };
 /* /\ ============== /\ *\
 |  /\ AFieldsManager /\  |
