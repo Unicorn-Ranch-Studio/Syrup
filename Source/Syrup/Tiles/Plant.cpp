f4d597977361a58422576f6cab766cdfb9f1362d
Liam Healey <56327428+LiamDHealey@users.noreply.github.com>
2022-09-29 21:41:09 -0600

    Update for wrapper

--- Source/Syrup/Tiles/Plant.cpp
+++ Source/Syrup/Tiles/Plant.cpp
@@ -173,7 +173,7 @@ void APlant::ReceiveEffectTrigger(const ETileEffectTriggerType TriggerType, cons
 		GetComponents(UTileEffect::StaticClass(), Components);
 		for (UActorComponent* EachComponent : Components)
 		{
-			Cast<UTileEffect>(EachComponent)->Affect(TriggerType, EffectedLocations);
+			Cast<UTileEffect>(EachComponent)->ActivateEffect(TriggerType, EffectedLocations);
 		}
 	}
 }
