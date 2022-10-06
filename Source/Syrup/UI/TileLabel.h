8c75e600ae1cdf7e8d0994ded89254b1d39b8375
Liam Healey <56327428+LiamDHealey@users.noreply.github.com>
2022-10-05 22:13:44 -0600

    Create tile label file

---
+++ Source/Syrup/UI/TileLabel.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "TileLabel.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SYRUP_API UTileLabel : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
